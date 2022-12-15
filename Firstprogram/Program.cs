7b552640ae6a67dc187a9a7128ce7d26bef3072a
yuiver <dbals1523@gmail.com>
2022-12-15 16:46:15 +0900

    Message I Want

---
+++ Firstprogram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System ;
+
+namespace Firstprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
